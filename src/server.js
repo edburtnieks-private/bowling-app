eac2ff5a74af1b7527085a2dacf092f765cc79d9
Ed Burtnieks <edburtnieks@gmail.com>
2019-07-31 11:48:18 +0300

    Delete Procfile, edit mongo db atlas user password

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
 const MONGO_DB_CONNECTION_STRING = dev
 	? 'mongodb://localhost/bowling-app'
-	: 'mongodb+srv://edburtnieks:Ao00Ia^Ulp@1C5Z@edburtnieks-opf8r.mongodb.net/bowling-app?retryWrites=true&w=majority'
+	: 'mongodb+srv://edburtnieks:IHExTtlVDs230S4w@edburtnieks-opf8r.mongodb.net/bowling-app?retryWrites=true&w=majority'
 
 mongoose.connect(MONGO_DB_CONNECTION_STRING, {
   useNewUrlParser: true
